ffacb780164b8f5f80994033e07f164b018724c2
Eric <ericc@example.com>
2026-04-01 20:34:26 +1100

    Implement getArnieQuotes function with Promise.all for concurrent HTTP requests

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,22 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const promises = urls.map(async (url) => {
+    try {
+      const response = await httpGet(url);
+      const body = JSON.parse(response.body);
+      
+      if (response.status === 200) {
+        return { 'Arnie Quote': body.message };
+      } else {
+        return { 'FAILURE': body.message };
+      }
+    } catch (error) {
+      return { 'FAILURE': error.message };
+    }
+  });
+  
+  return Promise.all(promises);
 };
 
 module.exports = {
